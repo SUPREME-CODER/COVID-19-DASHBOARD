3b2660006392095f1d90c79d2d4b4721783bd9df
Deep Doshi <39170964+SUPREME-CODER@users.noreply.github.com>
2020-09-01 09:48:12 +0530

    Delete urls.py

--- covid_UI/firstUI/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path(r'', views.analysis, name = 'analysis-home'),
-    path(r'singleCountry/', views.countryAnalysis, name = 'analysis-country'),
-    # path('', views.about, name = 'dev')
-]
